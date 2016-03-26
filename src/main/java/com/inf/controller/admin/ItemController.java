fe6ed4ad365153725adbe60a1523eedde0987024
nbchicong <nbchicong@gmail.com>
2016-03-26 07:34:29 +0700

    support category list in page admin/itemView, support list item

--- src/main/java/com/inf/controller/admin/ItemController.java
+++ src/main/java/com/inf/controller/admin/ItemController.java
@@ -1,5 +1,6 @@
 package com.inf.controller.admin;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.inf.data.SearchWrapper;
+import com.inf.dao.CategoryItemDAO;
 import com.inf.dao.ItemDAO;
 import com.inf.data.Result;
 import com.inf.model.ItemEntity;
@@ -23,17 +27,34 @@ public class ItemController {
 	
 	@Autowired
 	ItemDAO dao;
+
+  @Autowired
+  CategoryItemDAO categoryDAO;
 	
 	public ItemController(){}
-	
-	/**
-	 * Method will be called in initial page load at GET /admin/item
-	 * */
-	@RequestMapping(value = "/admin/item", method = RequestMethod.GET)
-	public String setupForm(Model model) {
-		return "admin/itemView";
-	}
-	
+
+  /**
+   * Method will be called in initial page load at GET /admin/item
+   */
+  @RequestMapping(value = "/admin/item", method = RequestMethod.GET)
+  public ModelAndView setupForm(Model model) {
+    ModelAndView modelAndView = new ModelAndView("admin/itemView");
+    modelAndView.addObject("cateList", categoryDAO.query());
+    return modelAndView;
+  }
+
+  @RequestMapping(value = "/admin/item/list", method = RequestMethod.GET)
+  public @ResponseBody SearchWrapper<ItemEntity> getSearchResultViaAjax(HttpServletRequest request) {
+    String category = request.getParameter("category");
+    if (StringUtils.hasLength(category)) {
+      return dao.findByCategory(Long.parseLong(category));
+    }
+    SearchWrapper<ItemEntity> results = new SearchWrapper<ItemEntity>();
+    List<ItemEntity> listItem = dao.query();
+    results.setList(listItem);
+    results.setTotal(listItem.size());
+    return results;
+  }
 	@RequestMapping(value = "/admin/item/save", method = RequestMethod.POST, consumes={MediaType.APPLICATION_JSON_VALUE})
 	public @ResponseBody ItemEntity save(@RequestBody ItemEntity item, HttpServletRequest request) {
 		return dao.save(item);
