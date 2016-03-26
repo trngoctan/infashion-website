ee350596d0d9d62d585135b295fdedb7a42068ea
tntan <trngoctan@gmail.com>
2016-03-26 11:56:53 +0700

    update

--- src/main/java/com/inf/model/CategoryItemEntity.java
+++ src/main/java/com/inf/model/CategoryItemEntity.java
@@ -16,7 +16,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table (name="category_item")
+@Table(name="category_item")
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class CategoryItemEntity implements Serializable {
 
